f8aa92119c6421af390c9e6c3a30546bab041e5f
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-10-08 15:34:11 +0100

    All words can be processed in Node

--- collator.js
+++ collator.js
@@ -68,142 +68,104 @@ const convertInputToOutputData = (allInputRows) => {
 //// (Divided into functions for easier commenting-out when debugging.)
 ////
 
-// if (typeof require !== 'undefined') {
+if (typeof require !== 'undefined') {
 
-// 	const fs = require('fs');
+	const fs = require('fs');
 
-// 	const runAllWords = () => {
+	const runAllWords = () => {
 
-// 		//// Input data look like "vocābulōrum\tvocābulum\rexcellentium\texcellēns excellō\r"
-// 		const inputFileUrl =
-// 			'C:/Users/Duncan Ritchie/Documents/Code/velutSideAssets/Excel/words/velut-words-word-and-lemmata.txt';
-// 		//// Output data are generated in batches & each batch is written to a file.
-// 		//// This allows me to track the output in Git without tracking a huge file.
-// 		const getOutputFileUrlForBatch = (batchNumber) =>
-// 			`C:/Users/Duncan Ritchie/Documents/Code/velutSideAssets/Json/words-from-generator_mongo_batch${batchNumber}.json`;
-// 		const batchSize = 50_000;
-// 		//// The output batches are concatenated into one file, for Git to ignore and me to import to MongoDB.
-// 		const outputFileUrl =
-// 			'C:/Users/Duncan Ritchie/Documents/Code/velutSideAssets/Json/words-from-generator_mongo.json';
-// 		//// For regression testing, I have a file of expected output, that the actual output is compared against.
-// 		const expectedOutputFileUrl =
-// 			'C:/Users/Duncan Ritchie/Documents/Code/velutSideAssets/Json/expected-words_mongo.json';
+		//// Input data look like "vocābulōrum\tvocābulum\rexcellentium\texcellēns excellō\r"
+		const inputFileUrl =
+			'C:/Users/Duncan Ritchie/Documents/Code/velutSideAssets/Excel/words/velut-words-word-and-lemmata.txt';
+		//// Output data are written to a file.
+		const outputFileUrl =
+			'C:/Users/Duncan Ritchie/Documents/Code/velutSideAssets/Json/lemmata-from-collator_mongo.json';
+		//// For regression testing, I have a file of expected output, that the actual output is compared against.
+		// const expectedOutputFileUrl =
+		// 	'C:/Users/Duncan Ritchie/Documents/Code/velutSideAssets/Json/expected-words_mongo.json';
 
-// 		try {
-// 			let batchFilepaths = [];
+		try {
+			const generateOutputAndSave = () => {
+				console.time('generatingOutput');
 
-// 			const generateOutputAndSaveInBatches = () => {
-// 				console.time('generatingOutput');
+				const data = fs.readFileSync(inputFileUrl, 'utf8');
+				const inputRows = data.split('\n');
 
-// 				const data = fs.readFileSync(inputFileUrl, 'utf8');
-// 				const inputRows = data.split('\n');
+				const output = convertInputToOutputData(inputRows);
 
-// 				//// Eg [1,2,3,4,5,6,7], 2 => [[1,2],[3,4],[5,6],[7]]
-// 				// from https://stackoverflow.com/a/54029307
-// 				const splitArrayIntoBatches = (array, size) =>
-// 					array.length > size
-// 						? [array.slice(0, size), ...splitArrayIntoBatches(array.slice(size), size)]
-// 						: [array];
-// 				const inputRowsBatched = splitArrayIntoBatches(inputRows, batchSize);
+				fs.writeFileSync(outputFileUrl, JSON.stringify(output));
 
-// 				let outputRowsBatched = [];
-// 				inputRowsBatched.forEach((batch, index, array) => {
-// 					const outputBatch = convertInputToOutputData(batch);
-// 					outputRowsBatched.push([...outputBatch]);
-// 				});
+				console.log('Output all data! See your file at ' + outputFileUrl);
+				console.timeEnd('generatingOutput');
+			}
 
-// 				batchFilepaths = outputRowsBatched
-// 					.map((batch, _) => batch.join('\n'))
-// 					.map((batch, batchNumber) => {
-// 						const filepath = getOutputFileUrlForBatch(batchNumber)
-// 						fs.writeFileSync(filepath, batch);
-// 						return filepath;
-// 					});
-// 				console.log('Output all data! See your file at ' + outputFileUrl + ' or ' + batchFilepaths);
+			// const checkAgainstExpected = () => {
+			// 	console.time('checkingOutput');
 
-// 				console.timeEnd('generatingOutput');
-// 			}
+			// 	const outputRows = fs.readFileSync(outputFileUrl, 'utf8').split('\n');
 
-// 			const concatenateBatches = () => {
-// 				console.time('concatenatingOutput');
+			// 	const expectedOutput = fs.readFileSync(expectedOutputFileUrl, 'utf8');
+			// 	const expectedOutputRows = expectedOutput.split('\n');
 
-// 				let output = '';
-// 				batchFilepaths.forEach(filename => {
-// 					output += fs.readFileSync(filename, 'utf8') + '\n';
-// 				})
-// 				fs.writeFileSync(outputFileUrl, output);
+			// 	let errorCount = 0;
+			// 	let lastWordSeen = '';
+			// 	for (
+			// 		let i = 0;
+			// 		i < outputRows.length && i < expectedOutputRows.length;
+			// 		i++
+			// 	) {
+			// 		if (outputRows[i].startsWith('"Word":')) {
+			// 			lastWordSeen = outputRows[i];
+			// 		}
 
-// 				console.timeEnd('concatenatingOutput');
-// 			}
+			// 		if (outputRows[i] === expectedOutputRows[i]) {
+			// 			// console.log('Yay!');
+			// 		} else {
+			// 			// if (
+			// 			// 	// !outputRows[i].startsWith('"Scansion"')
+			// 			// 	!lastWordSeen.startsWith('"Word": "coic') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "caelit') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "coiēns"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "conlātaque"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "deiēns"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "dein"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "deinde"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "hymenaeus"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "ignōrātiō') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "introiēns"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "iūsiūrandum"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "īnspectemque"') &&
+			// 			// 	!lastWordSeen.startsWith('"Word": "nūmin') &&
+			// 			// 	!lastWordSeen.includes('nf') &&
+			// 			// 	!lastWordSeen.includes('ifer') &&
+			// 			// 	!lastWordSeen.includes('iger') &&
+			// 			// 	!outputRows[i].startsWith('"LemmaCount"') &&
+			// 			// 	!outputRows[i].startsWith('"IsFitForDactyl"') &&
+			// 			// 	!outputRows[i].startsWith('"Uncompounded"')
+			// 			// 	// !outputRows[i].startsWith('"RhymeConsonants"')
+			// 			// ) {
+			// 			errorCount++;
+			// 			console.error({
+			// 				message: `Mismatch at line ${i}`,
+			// 				excelSays: expectedOutputRows[i],
+			// 				javascriptSays: outputRows[i],
+			// 				for: lastWordSeen,
+			// 			});
+			// 			// }
+			// 		}
+			// 	}
+			// 	console.warn(`There were ${errorCount} mismatches.`);
 
-// 			const checkAgainstExpected = () => {
-// 				console.time('checkingOutput');
+			// 	console.timeEnd('checkingOutput');
+			// };
 
-// 				const outputRows = fs.readFileSync(outputFileUrl, 'utf8').split('\n');
+			generateOutputAndSave();
+			// checkAgainstExpected();
 
-// 				const expectedOutput = fs.readFileSync(expectedOutputFileUrl, 'utf8');
-// 				const expectedOutputRows = expectedOutput.split('\n');
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-// 				let errorCount = 0;
-// 				let lastWordSeen = '';
-// 				for (
-// 					let i = 0;
-// 					i < outputRows.length && i < expectedOutputRows.length;
-// 					i++
-// 				) {
-// 					if (outputRows[i].startsWith('"Word":')) {
-// 						lastWordSeen = outputRows[i];
-// 					}
-
-// 					if (outputRows[i] === expectedOutputRows[i]) {
-// 						// console.log('Yay!');
-// 					} else {
-// 						// if (
-// 						// 	// !outputRows[i].startsWith('"Scansion"')
-// 						// 	!lastWordSeen.startsWith('"Word": "coic') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "caelit') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "coiēns"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "conlātaque"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "deiēns"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "dein"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "deinde"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "hymenaeus"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "ignōrātiō') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "introiēns"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "iūsiūrandum"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "īnspectemque"') &&
-// 						// 	!lastWordSeen.startsWith('"Word": "nūmin') &&
-// 						// 	!lastWordSeen.includes('nf') &&
-// 						// 	!lastWordSeen.includes('ifer') &&
-// 						// 	!lastWordSeen.includes('iger') &&
-// 						// 	!outputRows[i].startsWith('"LemmaCount"') &&
-// 						// 	!outputRows[i].startsWith('"IsFitForDactyl"') &&
-// 						// 	!outputRows[i].startsWith('"Uncompounded"')
-// 						// 	// !outputRows[i].startsWith('"RhymeConsonants"')
-// 						// ) {
-// 						errorCount++;
-// 						console.error({
-// 							message: `Mismatch at line ${i}`,
-// 							excelSays: expectedOutputRows[i],
-// 							javascriptSays: outputRows[i],
-// 							for: lastWordSeen,
-// 						});
-// 						// }
-// 					}
-// 				}
-// 				console.warn(`There were ${errorCount} mismatches.`);
-
-// 				console.timeEnd('checkingOutput');
-// 			};
-
-// 			generateOutputAndSaveInBatches();
-// 			concatenateBatches();
-// 			checkAgainstExpected();
-
-// 		} catch (err) {
-// 			console.error(err);
-// 		}
-// 	};
-
-// 	runAllWords();
-// }
+	runAllWords();
+}
